eebaded9de2f3ebd45900dfbb4ae289cec6863db
QuyenNNM0723 <quyennnm.work@gmail.com>
2024-12-23 00:11:41 +0700

    Cấu hình Router/Layout cho dự án

--- src/myAlert.js
+++
@@ -1,5 +0,0 @@
-const myAlert = () => {
-  alert("Hello, React!");
-};
-
-export default myAlert;
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// Không cần đăng nhập vẫn vào được
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// Đăng nhập mới vào được
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
